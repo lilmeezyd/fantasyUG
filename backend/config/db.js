a0d677b73a57a13ed6d544109329c3758994b45b
Denis Moini <denismoini09@gmail.com>
2024-11-26 01:01:31 +0300

    added-cors-be

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 const connectDB = async () => {
     const uri = `mongodb+srv://denismoini09:${process.env.PASSWORD}@fantasyug.zvubwkt.mongodb.net/ugandanDiba?retryWrites=true&w=majority&appName=fantasyUG`
+    console.log(process.env.PASSWORD)
     try {
         //const conn = await mongoose.connect(process.env.MONGO_URL)
         const conn = await mongoose.connect(uri, clientOptions)
