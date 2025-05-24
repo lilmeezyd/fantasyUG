323900923172516b301a0d9ace0468c1d40d8994
lilmeezyd <33873816+lilmeezyd@users.noreply.github.com>
2025-05-24 11:58:55 +0300

    Update Points.jsx

--- frontend/src/screens/Points.jsx
+++ frontend/src/screens/Points.jsx
@@ -26,10 +26,9 @@ const Points = () => {
     const a = []
     picksDetails?.picks?.forEach(x => {
         a.push(...x.livePicks)})
-    const minimum = 1
-    const maximum = a.length
-      //const minimum = Math.min(...a.map(x => x.matchday))
-      //const maximum = Math.max(...a.map(x => x.matchday))
+    
+      const minimum = Math.min(...a.map(x => x.matchday))
+      const maximum = Math.max(...a.map(x => x.matchday))
       setPageDetails(prev => ({...prev, page: maximum, min: minimum, max: maximum}))
   }, [matchdays, picksDetails])
   
