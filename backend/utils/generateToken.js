8d41870b062214e83dbfd659ca7201fed65ad448
Denis Moini <denismoini09@gmail.com>
2025-11-22 18:54:39 +0300

    added domain to cookie

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,9 @@ const generateToken = (res, userId) => {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        domain: "fantasyug-api.onrender.com",
+        path: "/",
         maxAge: 30 * 24 * 60 * 60 * 1000
     })
-}
+} 
 export default generateToken
