1a120b1458b12908b19cbd5e11b7cad814e28416
Denis Moini <denismoini09@gmail.com>
2025-12-05 13:43:13 +0300

    proxy backend through frontend

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -4,7 +4,7 @@ import { logout } from "../slices/authSlice";
 const isProd = import.meta.env.MODE === 'production';
 
 const baseUrl = isProd
-  ? import.meta.env.VITE_PROD_API_URL
+  ? "/"
   : import.meta.env.VITE_DEV_API_URL;
 
 const baseQuery = fetchBaseQuery({baseUrl, credentials: 'include'})
